d3b7de5296627a11c3788a60229af1e25cd83b37
Mikkel Overgaard Bitsch <201504472@post.au.dk>
2017-12-09 15:28:24 +0100

    Deleted buymenu

--- DAB_SmartGrid_ClientApp/SmartGrid_ServerApp/Address.cs
+++ DAB_SmartGrid_ClientApp/SmartGrid_ServerApp/Address.cs
@@ -2,6 +2,10 @@
 {
     public class Address
     {
-        
+        public int Id { get; set; }
+        public string StreetName { get; set; }
+        public string HouseNumber { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
     }
 }
