78867caef2906f7bc0cc71595ac1213d6578adee
Mikkel Overgaard Bitsch <201504472@post.au.dk>
2017-12-09 20:48:45 +0100

    Jacob lugter af ged

--- DAB_SmartGrid_ClientApp/SmartGrid_ServerApp/Transactions/CompletedTransaction.cs
+++ DAB_SmartGrid_ClientApp/SmartGrid_ServerApp/Transactions/CompletedTransaction.cs
@@ -2,12 +2,14 @@
 
 namespace SmartGrid_ServerApp
 {
-    public class PendingTransaction
+    public class CompletedTransaction
     {
         public string Id { get; set; }
         public string SellerId { get; set; }
         public string BuyerId { get; set; }
         public DateTime TransactionTime { get; set; }
         public string PowerAmount { get; set; }
+        public string TransactionPrice { get; set; }
+        public string PowerValueAtTransaction { get; set; }
     }
 }
--- DAB_SmartGrid_ClientApp/SmartGrid_ServerApp/Transactions/TransactionManager.cs
+++ DAB_SmartGrid_ClientApp/SmartGrid_ServerApp/Transactions/TransactionManager.cs
@@ -2,6 +2,15 @@
 {
     public class TransactionManager : ITransactionManager
     {
-        
+        private void StoreTransaction(PendingTransaction)
+        public void CreatePowerInventoryItem(Prosumer prosumer)
+        {
+            
+        }
+
+        public PendingTransaction CreateTransaction(Prosumer prosumer, PowerInventory inventory)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
